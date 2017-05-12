7a7b2ed58008da9af8cb7113f66604a46a1f58f0
will3 <will3.git@gmail.com>
2017-05-12 21:56:35 +0800

    WIP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const request = require('request');
 const cheerio = require('cheerio')
 const URL = require('url-parse');
 var Crawler = require("crawler");
+const _ = require('lodash');
+const util = require('util');
+var jaccard = require('jaccard');
 
 var ogParse = require('open-graph').parse;
 
@@ -9,9 +12,56 @@ class Video {
   constructor(params) {
     params = params || {};
     this.url = params.url;
-    this.swf = params.swf;
-    this.width = params.width;
-    this.height = params.height;
+    this.width = params.width || 0;
+    this.height = params.height || 0;
+    this.source = params.source;
+    this.pathSimilarity = 0;
+  }
+
+  get key() {
+    return [this.url, this.width, this.height, this.priority].join('\n');
+  }
+
+  updatePathSimilarity(originalUrlParts) {
+    const a = _.without(new URL(this.url).pathname.split('/'), '');
+    this.pathSimilarity = jaccard.index(a, originalUrlParts);
+  }
+
+  get priority() {
+    let value = 0;
+
+    if (this.source.element === 'meta') {
+      value += 100;
+    }
+
+    if (this.source.twitterPlayer) {
+      value += 50;
+    }
+
+    if (this.source.element === 'input') {
+      value += 10;
+    }
+
+    if (this.width > 0 && this.height > 0) {
+      value += 1;
+    }
+
+    const url = new URL(this.url);
+    if (url.pathname.indexOf('embed') !== -1) {
+      value += 1;
+    }
+
+    if (url.query.indexOf('autoplay') !== -1) {
+      value += 1;
+    }
+
+    value += this.pathSimilarity;
+
+    return value;
+  }
+
+  format() {
+    return this.priority + ' | ' + this.url + ' | ' + this.width + ' | ' + this.height + ' | ' + util.format(this.source);
   }
 }
 
@@ -23,6 +73,24 @@ const c = new Crawler({
 });
 
 const embed = (url, callback) => {
+  const phantom = require('phantom');
+
+  let content;
+  (async function() {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    await page.on("onResourceRequested", function(requestData) {
+      console.info('Requesting', requestData.url)
+    });
+
+    const status = await page.open('http://baidu.com');
+    console.log(status);
+
+    content = await page.property('content');
+
+    await instance.exit();
+  }());
+
   const result = {
     videos: [],
     url: url
@@ -32,7 +100,7 @@ const embed = (url, callback) => {
 }
 
 const readHTML = (url, result, callback) => {
-  console.log('fetching ' + url + '...');
+  console.log('fetching ' + url + ' ...');
   c.queue({
     uri: url,
     callback: function(err, res, done) {
@@ -49,44 +117,90 @@ const readHTML = (url, result, callback) => {
       try {
         console.log('processing og...');
         const og = ogParse(res.body);
-        console.log(og);
-        if (og.video != null) {
+        if (og.video != null && og.video.url) {
           if (Array.isArray(og.video.url)) {
             for (let i = 0; i < og.video.url.length; i++) {
-              result.videos.push(new Video({
+              const video = new Video({
                 url: og.video.url[i],
                 width: og.video.width[i],
-                height: og.video.height[i]
-              }));
+                height: og.video.height[i],
+                source: { element: 'meta' }
+              });
+              result.videos.push(video);
             }
           } else {
-            result.videos.push(new Video({
+            const video = new Video({
               url: og.video.url,
               width: og.video.width,
-              height: og.video.height
-            }));
+              height: og.video.height,
+              source: { element: 'meta' }
+            });
+            result.videos.push(video);
           }
         }
       } catch (err) {
         console.log('Open Graph error: ', err);
       }
 
-      if ($("meta[name='twitter:player']").length > 0) {
-        result.videos.push(new Video({
-          url: $("meta[name='twitter:player']").attr('content'),
-          width: $("meta[name='twitter:player:width']").attr('content'),
-          height: $("meta[name='twitter:player:height']").attr('content')
-        }));
+      const readContentOrValue = (element) => {
+        return element.attr('content') || element.attr('value');
       }
-    //       <meta name="twitter:player" content="https://www.pornhub.com/embed/ph58cccc07d4dbe">
-    // <meta name="twitter:player:width" content="640">
-    // <meta name="twitter:player:height" content="360">
+
+      if ($("meta[name='twitter:player']").length > 0) {
+        const video = new Video({
+          url: readContentOrValue($("meta[name='twitter:player']")),
+          width: readContentOrValue($("meta[name='twitter:player:width']")),
+          height: readContentOrValue($("meta[name='twitter:player:height']")),
+          source: { element: 'meta', twitterPlayer: true }
+        })
+        result.videos.push(video);
+      }
+
+      // Only found in Break so far...
+      if ($("meta[name='embed_video_url']").length > 0) {
+        const video = new Video({
+          url: readContentOrValue($("meta[name='embed_video_url']")),
+          thumbUrl: readContentOrValue($("meta[name='embed_video_thumb_url']")),
+          title: readContentOrValue($("meta[name='embed_video_title']")),
+          description: readContentOrValue($("meta[name='embed_video_description']")),
+          width: readContentOrValue($("meta[name='embed_video_width']")),
+          height: readContentOrValue($("meta[name='embed_video_height']")),
+          source: { element: 'meta' }
+        });
+        result.videos.push(video);
+      }
 
       const num = $('*').length;
       console.log('processing ' + num + ' elements...');
 
       $('*').each(function(i, selected) {
         readElement(this, selected, result);
+      });
+
+      const originalUrlParts = _.without(new URL(url).pathname.split('/'), '');
+
+      for (let i = 0; i < result.videos.length; i++) {
+        const video = result.videos[i];
+        video.updatePathSimilarity(originalUrlParts);
+      }
+
+      // Get unique videos
+      const map = {};
+      for (let i = 0; i < result.videos.length; i++) {
+        const video = result.videos[i];
+        map[video.key] = video;
+      }
+
+      const videos = [];
+      for (let key in map) {
+        videos.push(map[key]);
+      }
+
+      // This is for testing
+      videos.reverse();
+
+      result.videos = videos.sort((a, b) => {
+        return b.priority - a.priority;
       });
 
       callback(null, result);
@@ -96,22 +210,38 @@ const readHTML = (url, result, callback) => {
 
 const readElement = (element, selected, result) => {
   if (element.type === 'tag' && element.name === 'textarea') {
-    const html = $(element).html();
-    readString(html, result);
+    const html = cheerio(element).html();
+    readString(html, result, 'textarea');
   }
 
   if (element.type === 'script' && element.name === 'script') {
-    const html = $(element).html();
+    const html = cheerio(element).html();
     readScript(html, result);
+  }
+
+  if (element.type === 'tag' && element.name === 'iframe') {
+    const url = cheerio(element).attr('src');
+    if (url != null) {
+      const urlObj = new URL(value);
+      if (urlObj.host.length > 0) {
+        const width = cheerio(element).attr('width');
+        const height = cheerio(element).attr('height');
+        const source = { element: 'iframe' };
+        const video = new Video({ url, width, height, source });
+        result.videos.push(video);
+      }
+    }
   }
 
   for (name in element.attribs) {
     value = element.attribs[name];
-    readString(value, result);
+    source = { element: element.name, attr: name };
+
+    readString(value, result, source);
   }
 };
 
-const readString = (value, result) => {
+const readString = (value, result, source) => {
   const url = new URL(value);
 
   if (url.host.length > 0) {
@@ -119,10 +249,12 @@ const readString = (value, result) => {
     const ext = pathname.split('.').pop();
 
     if (ext === 'swf') {
+      let s = _.clone(source);
+      s.parsed = 'url';
       result.videos.push(new Video({
-        swf: url.href
+        url: url.href,
+        source: s
       }));
-      return;
     }
   }
 
@@ -133,11 +265,15 @@ const readString = (value, result) => {
   if (embed.length > 0) {
     const src = embed.attr('src');
     if (src != null) {
-      result.videos.push(new Video({
+      let s = _.clone(source);
+      s.parsed = 'embed';
+      const video = new Video({
         url: src,
         width: embed.attr('width'),
-        height: embed.attr('height')
-      }));
+        height: embed.attr('height'),
+        source: s
+      });
+      result.videos.push(video);
     }
   }
 
@@ -146,24 +282,32 @@ const readString = (value, result) => {
   if (iframe.length > 0) {
     const src = iframe.attr('src');
     if (src != null) {
-      result.videos.push(new Video({
+      let s = _.clone(source);
+      s.parsed = 'iframe';
+      const video = new Video({
         url: src,
         width: iframe.attr('width'),
-        height: iframe.attr('height')
-      }));
+        height: iframe.attr('height'),
+        source: s
+      });
+      result.videos.push(video);
     }
   }
 }
 
 const readScript = (script, result) => {
   const quotedStrings = (script.match(/"[^"\\]*(?:\\.[^"\\]*)*"/g) || []).forEach((string) => {
-    string = string.substring(1, string.length - 2);
-    readString(string, result);
+    string = string.substring(1, string.length - 1);
+    readString(string, result, {
+      element: 'script'
+    });
   });
 
   const singleQuotedStrings = (script.match(/'[^'\\]*(?:\\.[^'\\]*)*'/g) || []).forEach((string) => {
-    string = string.substring(1, string.length - 2);
-    readString(string, result);
+    string = string.substring(1, string.length - 1);
+    readString(string, result, {
+      element: 'script'
+    });
   });
 }
 module.exports = embed;
--- src/test/utils/test.js
+++ src/test/utils/test.js
@@ -1,13 +1,36 @@
 const embed = require('../../index');
-const validateResult = require('./validateresult');
 
-module.exports = (name, link) => {
+module.exports = (name, link, expectedLink) => {
   describe(name, function() {
     it(name, function(done) {
       this.timeout(60000);
       embed(link, function(err, result) {
-        validateResult(err, result);
-        console.log(result.videos);
+        if (err) { throw err; }
+
+        if (result.videos.length === 0) {
+          throw new Error('no way to embed ' + result.url);
+        }
+
+        const info = result.videos.map((video) => {
+          return video.format();
+        }).join('\n');
+        console.log('found ' + result.videos.length + ' source(s):');
+        console.log(info);
+
+        if (expectedLink !== undefined) {
+          if (expectedLink !== result.videos[0].url) {
+            throw new Error('first link does not match ' + expectedLink);
+          }
+
+          if (result.videos[0].priority === 0) {
+            throw new Error('expected link has a priority of 0: ' + expectedLink);            
+          }
+
+          if (result.videos[1] != null && result.videos[0].priority === result.videos[1].priority) {
+            throw new Error('second choice has the same priority as expected link: ' + expectedLink);
+          }
+        }
+
         done();
       });
     });
