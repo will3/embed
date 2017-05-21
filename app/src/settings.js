8ccb65dc9aac6cb0b53102aa1dfcd415c3dd3463
will3 <will3.git@gmail.com>
2017-05-21 21:22:02 +0800

    Fix production url

--- app/src/settings.js
+++ app/src/settings.js
@@ -1,14 +1,11 @@
 const settings = {
-	host: 'https://fourplay.herokuapp.com',
+	host: 'https://fourplayer.herokuapp.com',
 	mockFullScreen: false
 };
 
-if (process.env.NODE_ENV === 'development') {
-	settings.host = 'http://localhost:3001';
-}
-
 if (process.env.NODE_ENV !== 'development') {
 	settings.mockFullScreen = false;
+	settings.host = 'https://fourplayer.herokuapp.com';
 }
 
 export default settings;
